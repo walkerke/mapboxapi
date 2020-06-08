f151e6f8a3d0ca1fdc520ea07379e39ca2ddd63b
Kyle Walker <kwalkerdata@gmail.com>
2020-06-07 23:04:37 -0500

    support for route optimization API

--- R/route.R
+++ R/route.R
@@ -6,15 +6,15 @@
 #' @param profile One of "driving" (the default), "driving-traffic", "walking", or "cycling".
 #' @param output One of "sf" (the default), which returns an sf LINESTRING representing the route geometry, or "full", which returns the full request from the Directions API as a list.
 #' @param alternatives Whether or not to return alternative routes with your request. If TRUE, a list of up to 3 possible routes will be returned.
-#' @param annotations A comma-separated string of additional route metadata, which may include duration, distance, speed, and congestion. Must be used with overview = FULL.
-#' @param bearings bearings
+#' @param annotations A comma-separated string of additional route metadata, which may include duration, distance, speed, and congestion. Must be used with overview = "full".
+#' @param bearings A semicolon-delimited character string of bearings
 #' @param continue_straight continue_straight
 #' @param exclude Road types to exclude from your route; possible choices are \code{'toll'}, \code{'motorway'}, or \code{'ferry'}.  Defaults to NULL.
 #' @param geometries The route geometry format.  If \code{output = 'sf'}, you will get back an sf object and you should leave this blank.  If \code{output = 'full'}, the embedded route geometries will be \code{polyline} with five decimal place precision.  \code{'polyline6'} may also be specified.
 #' @param overview If left blank, defaults to \code{'simplified'} for simplified geometry; the other option is \code{'full'} which provides the most detailed geometry available.
 #' @param radiuses A character string with semicolon-separated radii that specify the distance (in meters) to snap each input coordinate to the road network.  Defaults to NULL.
 #' @param approaches A character string with semicolon-separated specifications for how to approach waypoints.  Options include \code{unrestricted} and \code{curb}.  Defaults to NULL which uses \code{unrestricted} for all waypoints.
-#' @param steps steps If TRUE, returns the route object split up into route legs with step-by-step instructions included.  If FALSE or NULL (the default), a single line geometry representing the full route will be returned.
+#' @param steps If TRUE, returns the route object split up into route legs with step-by-step instructions included.  If FALSE or NULL (the default), a single line geometry representing the full route will be returned.
 #' @param banner_instructions Whether or not to return banner objects; only available when \code{output = 'full'} and \code{steps = TRUE}.
 #' @param language The language of the returned instructions (defaults to English). Available language codes are found at \url{https://docs.mapbox.com/api/navigation/#instructions-languages}.  Only available when \code{steps = TRUE}.
 #' @param roundabout_exits If TRUE, adds instructions for roundabout entrance and exit.  Only available when \code{steps = TRUE}.
@@ -213,7 +213,7 @@ mb_directions <- function(input_data = NULL,
     if (roundabound_exits) {
       roundabout_exits <- 'true'
     } else {
-      roundabount_exits <- 'false'
+      roundabout_exits <- 'false'
     }
   }
 
@@ -238,13 +238,6 @@ mb_directions <- function(input_data = NULL,
       voice_units <- 'false'
     }
   }
-
-  # If the output is sf, the returned geometry should be 'geojson' so that it is easier
-  # to parse without additional dependencies
-  if (output == "sf") {
-
-  }
-
 
   request <- httr::GET(url = base,
                        query = list(
@@ -318,6 +311,197 @@ mb_directions <- function(input_data = NULL,
 
         route$distance <- content$routes$distance[.y] / 1000
         route$duration <- content$routes$duration[.y] / 60
+
+        return(route)
+      })
+
+      if (length(to_return) == 1) {
+        return(to_return[[1]])
+      } else {
+        return(to_return)
+      }
+    }
+  } else {
+    return(content)
+  }
+
+}
+
+
+#' Return an optimized route for a series of input coordinates
+#'
+#' @param input_data An input dataset of class \code{"sf"}, or a list of coordinate pairs of format \code{c(longitude, latitude)}.  Must be between 2 and 12 coordinate pairs.
+#' @param profile One of "driving" (the default), "driving-traffic", "walking", or "cycling".
+#' @param output One of "sf" (the default), which returns an sf LINESTRING representing the route geometry, or "full", which returns the full request from the Directions API as a list.
+#' @param source One of \code{"any"} (the default) or \code{"first"}.  If "any" is specified, any of the input coordinates may be used as the starting point.  If "first" is specified, the first coordinate will be used.
+#' @param destination One of \code{"any"} (the default) or \code{"last"}.  If "any" is specified, any of the input coordinates may be used as the ending point.  If "last" is specified, the last coordinate will be used.
+#' @param roundtrip If \code{TRUE} (the default), the route will start and end at the same point.
+#' @param annotations A comma-separated string of additional route metadata, which may include duration, distance, speed, and congestion. Must be used with overview = "full".
+#' @param approaches A character string with semicolon-separated specifications for how to approach waypoints.  Options include \code{unrestricted} and \code{curb}.  Defaults to NULL which uses \code{unrestricted} for all waypoints.
+#' @param bearings A semicolon-delimited character string of bearings.
+#' @param distributions A semicolon-delimited character string of number pairs that specifies pick-up and drop-off locations.  The first number indicates the index of the pick-up location, and the second number represents the index of the drop-off location.
+#' @param language The language of the returned instructions (defaults to English). Available language codes are found at \url{https://docs.mapbox.com/api/navigation/#instructions-languages}.  Only available when \code{steps = TRUE}.
+#' @param overview If left blank, defaults to \code{'simplified'} for simplified geometry; the other option is \code{'full'} which provides the most detailed geometry available.
+#' @param radiuses A character string with semicolon-separated radii that specify the distance (in meters) to snap each input coordinate to the road network.  Defaults to NULL.
+#' @param steps If TRUE, returns the route object split up into route legs with step-by-step instructions included.  If FALSE or NULL (the default), a single line geometry representing the full route will be returned.
+#' @param access_token Your Mapbox access token; set with \code{mb_access_token()}
+#'
+#' @return An sf object or list representing the optimization API response.
+#' @export
+mb_optimized_route <- function(input_data,
+                               profile = c("driving", "walking", "cycling",
+                                           "driving-traffic"),
+                               output = "sf",
+                               source = c("any", "first"),
+                               destination = c("any", "last"),
+                               roundtrip = TRUE,
+                               annotations = NULL,
+                               approaches = NULL,
+                               bearings = NULL,
+                               distributions = NULL,
+                               language = NULL,
+                               overview = "simplified",
+                               radiuses = NULL,
+                               steps = NULL,
+                               access_token = NULL) {
+
+  profile <- match.arg(profile)
+  source <- match.arg(source)
+  destination <- match.arg(destination)
+
+  if (is.null(access_token)) {
+    # Use public token first, then secret token
+    if (Sys.getenv("MAPBOX_PUBLIC_TOKEN") != "") {
+      access_token <- Sys.getenv("MAPBOX_PUBLIC_TOKEN")
+    } else {
+      if (Sys.getenv("MAPBOX_SECRET_TOKEN" != "")) {
+        access_token <- Sys.getenv("MAPBOX_SECRET_TOKEN")
+      } else {
+        stop("A Mapbox access token is required.  Please locate yours from your Mapbox account.", call. = FALSE)
+      }
+    }
+  }
+
+  # If input_data is an sf object, process it accordingly
+  if (!is.null(input_data)) {
+    if (any(grepl("^sf", class(input_data)))) {
+
+      if (unique(sf::st_geometry_type(input_data)) != "POINT") {
+        input_data <- suppressWarnings(sf::st_centroid(input_data))
+      }
+
+      input_data <- sf::st_transform(input_data, 4326)
+
+      coords <- sf::st_coordinates(input_data) %>%
+        as.data.frame() %>%
+        purrr::transpose()
+
+      formatted_coords <- purrr::map(coords, ~{
+        paste0(.x, collapse = ",")
+      }) %>%
+        unlist() %>%
+        paste0(collapse = ";")
+    } else if ("list" %in% class(input_data)) {
+
+      formatted_coords <- map(input_data, ~{
+        # If list element is an address, geocode it
+        if (class(.x) == "character") {
+          cxy <- mb_geocode(.x)
+        } else {
+          cxy <- .x
+        }
+        paste0(cxy, collapse = ",")
+      }) %>%
+        unlist() %>%
+        paste0(collapse = ";")
+
+    }
+  }
+
+  # Assemble the request
+  base <- sprintf("https://api.mapbox.com/optimized-trips/v1/mapbox/%s/%s",
+                  profile, formatted_coords)
+
+  # Account for boolean-to-string logic if applicable
+  if (!is.null(steps)) {
+    if (steps) {
+      steps <- 'true'
+    }
+  } else {
+    steps <- 'false'
+  }
+
+
+  if (roundtrip) {
+    roundtrip <- 'true'
+  } else {
+    roundtrip <- 'false'
+  }
+
+
+  request <- httr::GET(url = base,
+                       query = list(
+                         access_token = access_token,
+                         annotations = annotations,
+                         bearings = bearings,
+                         destination  = destination,
+                         distributions = distributions,
+                         geometries = "geojson",
+                         overview = "simplified",
+                         radiuses = radiuses,
+                         approaches = approaches,
+                         steps = steps,
+                         language = language,
+                         source = source,
+                         roundtrip = roundtrip))
+
+
+  if (request$status_code != 200) {
+    pull <- jsonlite::fromJSON(content)
+    stop(pull$message, call. = FALSE)
+  }
+
+  content <- httr::content(request, as = "text") %>%
+    jsonlite::fromJSON()
+
+  if (output == "sf") {
+
+    if (steps == 'true') {
+
+      to_return <- purrr::map(content$trips$legs[[1]]$steps, ~{
+
+        route <- purrr::map(.x$geometry$coordinates, function(r) {
+          r %>%
+            sf::st_linestring() %>%
+            sf::st_sfc(crs = 4326) %>%
+            sf::st_sf()
+        }) %>%
+          dplyr::bind_rows()
+
+        route$distance <- .x$distance / 1000
+        route$duration <- .x$duration / 60
+        route$instruction <- .x$maneuver$instruction
+
+        return(route)
+      }) %>%
+        dplyr::bind_rows()
+
+      if (length(to_return) == 1) {
+        return(to_return[[1]])
+      } else {
+        return(to_return)
+      }
+
+    } else {
+
+      to_return <- purrr::imap(content$trips$geometry$coordinates, ~{
+        route <- .x %>%
+          sf::st_linestring() %>%
+          sf::st_sfc(crs = 4326) %>%
+          sf::st_sf()
+
+        route$distance <- content$trips$distance[.y] / 1000
+        route$duration <- content$trips$duration[.y] / 60
 
         return(route)
       })
