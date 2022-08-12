152150efe794fd1a16378128f184c0f2973fd335
Kyle Walker <kwalkerdata@gmail.com>
2022-08-11 21:52:52 -0500

    successful end-to-end MTS functionality supported

--- R/mts.R
+++ R/mts.R
@@ -9,6 +9,17 @@ make_newline_geojson <- function(data, file_loc) {
 
 }
 
+#' Create a Mapbox tileset source from a sf object using the Mapbox Tiling Service API
+#'
+#' The \code{mts_create_source()} function can be used to create, append to, or replace an existing tileset source.
+#'
+#' @param data An input simple features object
+#' @param tileset_id The tileset ID. If the tileset ID already exists in your Mapbox account, this function will overwrite the existing source with a new source.
+#' @param username Your Mapbox username
+#' @param access_token Your Mapbox access token with secret scope. Install with \code{mb_access_token()} after you retrieve it from your Mapbox account.
+#'
+#' @return A list of the MTS API's responses, including the name of the tileset source in your Mapbox account.  You'll use this name to build a MTS recipe.
+#' @export
 mts_create_source <- function(data, tileset_id, username,
                               access_token = NULL) {
 
@@ -56,22 +67,79 @@ mts_create_source <- function(data, tileset_id, username,
 }
 
 
-mts_make_recipe <- function(..., name) {
+#' List tileset sources in your Mapbox account
+#'
+#' @param username Your Mapbox username
+#' @param access_token Your Mapbox access token with secret scope.
+#'
+#' @return A data frame containing information on your tileset sources.
+#' @export
+mts_list_sources <- function(username,
+                             access_token = NULL) {
+
+  access_token <- get_mb_access_token(access_token, secret_required = TRUE)
+
+
+
+  request <- httr::GET(
+    url = sprintf("https://api.mapbox.com/tilesets/v1/sources/%s", username),
+    query = list(access_token = access_token)
+  )
+
+  response <- request %>%
+    httr::content(as = "text") %>%
+    jsonlite::fromJSON()
+
+  if (request$status_code != "200") {
+    stop(sprintf("Request failed: your error message is %s", response),
+         call. = FALSE
+    )
+  }
+
+  return(response)
+
+}
+
+
+mts_make_recipe <- function(...) {
 
   # Create recipe skeleton
   # Make this easier in the future
   recipe <- list(
-    recipe = list(
       version = 1,
       layers = list(...)
-    ),
-    name = name
+    )
+  # Convert the recipe to JSON
+  # recipe_json <- jsonlite::toJSON(recipe, auto_unbox = TRUE)
+
+  return(recipe)
+
+}
+
+mts_validate_recipe <- function(recipe,
+                                access_token = NULL) {
+
+  access_token <- get_mb_access_token(access_token)
+
+  request <- httr::PUT(
+    url = "https://api.mapbox.com/tilesets/v1/validateRecipe",
+    body = recipe,
+    encode = "json",
+    query = list(access_token = access_token),
+    httr::content_type_json()
   )
 
-  # Convert the recipe to JSON
-  recipe_json <- jsonlite::toJSON(recipe, auto_unbox = TRUE)
+  response <- request %>%
+    httr::content(as = "text") %>%
+    jsonlite::fromJSON()
 
-  return(recipe_json)
+  if (request$status_code != "200") {
+    stop(sprintf("Request failed: your error message is %s", response),
+         call. = FALSE
+    )
+  }
+
+  return(response)
 
 }
 
@@ -88,19 +156,18 @@ mts_create_tileset <- function(recipe,
 
   url <- sprintf("https://api.mapbox.com/tilesets/v1/%s", tileset)
 
-  # Make a temporary directory to store the JSON recipe
-  tmp <- tempdir()
-
-  # Write the dataset to the temporary directory
-  out_file <- file.path(tmp, "recipe.json")
-
-  writeLines(recipe, out_file)
+  # Prepare the request body
+  req_body <- list(
+    recipe = recipe,
+    name = request_name
+  )
 
   request <- httr::POST(
     url = url,
-    body = list(file = httr::upload_file(out_file)),
+    body = req_body,
+    encode = "json",
     query = list(access_token = access_token),
-    httr::add_headers("Content-Type:application/json")
+    httr::content_type_json()
   )
 
   response <- request %>%
@@ -113,6 +180,38 @@ mts_create_tileset <- function(recipe,
     )
   }
 
+  return(response)
+
+}
+
+
+mts_publish_tileset <- function(tileset_name,
+                                username,
+                                access_token = NULL) {
+
+  access_token <- get_mb_access_token(access_token, secret_required = TRUE)
+
+  tileset <- sprintf("%s.%s", username, tileset_name)
+
+  url <- sprintf("https://api.mapbox.com/tilesets/v1/%s/publish",
+                 tileset)
+
+  request <- httr::POST(
+    url = url,
+    query = list(access_token = access_token)
+  )
+
+  response <- request %>%
+    httr::content(as = "text") %>%
+    jsonlite::fromJSON()
+
+  if (request$status_code != "200") {
+    stop(sprintf("Request failed: your error message is %s", response),
+         call. = FALSE
+    )
+  }
+
+  return(response)
 
 }
 
