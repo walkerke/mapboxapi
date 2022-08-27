c20f8cecd659a4efc7cbd9fdf49317380b520a7e
Eli Pousson <eli.pousson@gmail.com>
2022-08-26 21:41:23 -0400

    fix: add missing distance units to dist_unit_options
    - fix: add missing distance units (US_survey_foot, US_survey_feet, US_survey_yard, US_survey_mile, and US_statute_mile) to dist_unit_options

--- data-raw/internal.R
+++ data-raw/internal.R
@@ -1,7 +1,7 @@
 ## code to prepare `internal` dataset goes here
 
-# See {overedge} package repo for reproducible code to generate dist_unit_options
-# https://github.com/elipousson/overedge/blob/c4428036201270a70654b67121872a08d061c22e/data-raw/data.R#L248-L306
-dist_unit_options <- overedge:::dist_unit_options
+# See {sfext} package repo for reproducible code to generate dist_unit_options
+# https://github.com/elipousson/sfext/blob/main/data-raw/units.R
+dist_unit_options <- sfext::dist_unit_options
 
 usethis::use_data(dist_unit_options, internal = TRUE, overwrite = TRUE)
